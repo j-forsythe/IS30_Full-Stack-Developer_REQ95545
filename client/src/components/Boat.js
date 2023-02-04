42aee1012a234cf5d85f12eef7f54ec50d0b0908
Jill Forsythe <jill@legible.com>
2023-02-03 17:19:13 -0800

    feat(client): update boat with drag and drop

--- client/src/components/Boat.js
+++ client/src/components/Boat.js
@@ -1,6 +1,5 @@
 import {
     Card,
-    CardActionArea,
     CardActions,
     CardContent,
     IconButton,
@@ -9,14 +8,12 @@ import {
 import DeleteIcon from '@mui/icons-material/Delete'
 import React from 'react'
 
-const Boat = ({ data }) => {
+const Boat = ({ data, innerRef, ...props }) => {
     return (
-        <Card sx={{ margin: 2 }}>
-            <CardActionArea>
-                <CardContent>
-                    <Typography variant="h6">{data.name}</Typography>
-                </CardContent>
-            </CardActionArea>
+        <Card sx={{ margin: 2 }} ref={innerRef} {...props}>
+            <CardContent>
+                <Typography variant="h6">{data.name}</Typography>
+            </CardContent>
             <CardActions>
                 <IconButton>
                     <DeleteIcon />
--- client/src/components/Swimlanes.js
+++ client/src/components/Swimlanes.js
@@ -1,14 +1,48 @@
 import { Grid, Paper, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import Boat from './Boat'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 const Swimlanes = () => {
     const [statuses, setStatuses] = useState([])
     const [boats, setBoats] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
+    const updateBoat = async (data) => {
+        await fetch('/api/update-boat', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+    }
+
+    const onDragEnd = (result) => {
+        const { destination, source, draggableId } = result
+        if (source.droppableId !== destination.droppableId) {
+            let data = {
+                status_id: Number(destination.droppableId),
+                id: Number(draggableId),
+            }
+            updateBoat(data)
+            const newList = boats.map((boat) => {
+                if (boat.id === data.id) {
+                    const updatedItem = {
+                        ...boat,
+                        status_id: data.status_id,
+                    }
+                    return updatedItem
+                }
+                return boat
+            })
+
+            setBoats(newList)
+        }
+    }
+
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchStatuses = async () => {
             setIsLoading(true)
             const result = await fetch('/api/get-statuses').then((res) =>
                 res.json(),
@@ -16,11 +50,11 @@ const Swimlanes = () => {
             setStatuses(result)
             setIsLoading(false)
         }
-        fetchData()
+        fetchStatuses()
     }, [])
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchBoats = async () => {
             setIsLoading(true)
             const result = await fetch('/api/get-boats').then((res) =>
                 res.json(),
@@ -28,39 +62,73 @@ const Swimlanes = () => {
             setBoats(result)
             setIsLoading(false)
         }
-        fetchData()
+        fetchBoats()
     }, [])
 
     if (isLoading) return 'Loading'
 
     return (
-        <Grid
-            container
-            spacing={2}
-            columns={{ xs: 1, md: 4 }}
-            sx={{ height: '75vh' }}
-        >
-            {statuses &&
-                statuses.map((status) => (
-                    <Grid item xs={4} md={1} key={status.id}>
-                        <Paper sx={{ height: '100%' }}>
-                            <Typography
-                                align="center"
-                                variant="h5"
-                                sx={{ paddingTop: 1 }}
-                            >
-                                {status.title}
-                            </Typography>
-                            {boats &&
-                                boats
-                                    .filter(
-                                        (boat) => boat.status_id === status.id,
-                                    )
-                                    .map((boat) => <Boat data={boat} />)}
-                        </Paper>
-                    </Grid>
-                ))}
-        </Grid>
+        <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
+            <Grid
+                container
+                spacing={2}
+                columns={{ xs: 1, md: 4 }}
+                sx={{ height: '75vh' }}
+            >
+                {statuses &&
+                    statuses.map((status) => (
+                        <Droppable droppableId={`${status.id}`} key={status.id}>
+                            {(provided, snapshot) => (
+                                <Grid
+                                    item
+                                    xs={4}
+                                    md={1}
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                >
+                                    <Paper sx={{ height: '100%' }}>
+                                        <Typography
+                                            align="center"
+                                            variant="h5"
+                                            sx={{ paddingTop: 1 }}
+                                        >
+                                            {status.title}
+                                        </Typography>
+                                        {boats &&
+                                            boats
+                                                .filter(
+                                                    (boat) =>
+                                                        boat.status_id ===
+                                                        status.id,
+                                                )
+                                                .map((boat, index) => (
+                                                    <Draggable
+                                                        draggableId={`${boat.id}`}
+                                                        index={index}
+                                                        key={boat.id}
+                                                    >
+                                                        {(
+                                                            provided,
+                                                            snapshot,
+                                                        ) => (
+                                                            <Boat
+                                                                data={boat}
+                                                                innerRef={
+                                                                    provided.innerRef
+                                                                }
+                                                                {...provided.draggableProps}
+                                                                {...provided.dragHandleProps}
+                                                            />
+                                                        )}
+                                                    </Draggable>
+                                                ))}
+                                    </Paper>
+                                </Grid>
+                            )}
+                        </Droppable>
+                    ))}
+            </Grid>
+        </DragDropContext>
     )
 }
 
